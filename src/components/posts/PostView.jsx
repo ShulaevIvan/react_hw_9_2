766a88e09e0c41161d0d8d86c36fbf7856806d99
Shulaev Ivan <mail@ntdservice.ru>
2023-05-31 23:29:36 +0300

    fix bugs

--- src/components/posts/PostView.jsx
+++ src/components/posts/PostView.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useRef, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Context } from "../../context";
 import { useNavigate } from "react-router-dom";
@@ -8,7 +8,12 @@ import "moment/locale/ru";
 
 
 const PostView = () => {
-    const initialState = { editMode: false };
+    const initialState = { 
+        editMode: false, 
+        postId: undefined,
+        postDate: undefined,
+        editTextAreaRef: useRef()
+    };
     const [viewState, setViewState] = useState(initialState);
     const param = useParams();
     const id = param.id;
@@ -36,14 +41,52 @@ const PostView = () => {
     const editPostHandler = () => {
         setViewState(prevState => ({
             ...prevState,
+            postId: prevState.postId = targetPost.id,
             editMode: prevState.editMode = true,
-        }))
+        }));
     };
+
+    const savePostHandler = () => {
+        setViewState(prevState => ({
+            ...prevState,
+            editMode: prevState.editMode = false,
+        }));
+    };
+
+    const cancelBtnHandler = () => {
+        setViewState(prevState => ({
+            ...prevState,
+            editMode: prevState.editMode = false,
+        }));
+        navigate(`/posts/${targetPost.id}`);
+    }
+
+    useEffect(() => {
+        if (viewState.editMode) viewState.editTextAreaRef.current.value = targetPost.content;
+    }, [viewState.editMode])
 
     if (viewState.editMode) {
         return (
             <React.Fragment>
-                Edit mode
+                <div className="post-wrapper">
+                   <div className="post-edit-title">
+                        <h4>Редактировать пост</h4>
+                        <span className="edit-rm-btn" onClick={cancelBtnHandler}></span>
+                   </div>
+                   <div className="post-author-wrapper">
+                        <div className="post-author-img"><img src="https://imgholder.ru/50x50/8493a8/adb9ca&text=IMAGE+HOLDER&font=kelson" alt="#" /></div>
+                        <div className="post-autgor-name">
+                            <span className="post-author">Author</span>
+                            <span className="post-createted">{targetPost ? moment(targetPost.created).fromNow() : null}</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="post-content-edit-wrap">
+                    <textarea className="post-content-edit" ref={viewState.editTextAreaRef} />
+                </div>
+                <div className="post-controls-wrapper">
+                    <a href="#" className="post-control-edit-save" onClick={savePostHandler}>Сохранить</a>
+                </div>
             </React.Fragment>
         )
     }
@@ -58,7 +101,7 @@ const PostView = () => {
                             <span className="post-createted">{targetPost ? moment(targetPost.created).fromNow() : null}</span>
                         </div>
                     </div>
-                    <div className="post-content">
+                    <div className="post-content" ref={viewState.postContentRef}>
                         {targetPost ? targetPost.content : null}
                     </div>
                     <div className="post-controls-wrapper">
